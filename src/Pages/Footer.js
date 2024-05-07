b66aefda986ce1231e2979d173cabc9472370273
Abhay Jain <pr245998@dal.ca>
2024-05-06 21:33:35 -0300

    commit-3

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
-        <div className="max-w-4xl px-8 mx-auto text-center">
-          <p>&copy; 2024 Journey Beyond Limits. All rights reserved.</p>
-        </div>
+      <div className="max-w-4xl px-8 mx-auto text-center">
+        <p>&copy; 2024 Journey Beyond Limits. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
